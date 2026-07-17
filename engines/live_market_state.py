23ffddaeeb1721d7534bba88dc5a66675e33f4f9
shree1316-cyber <shree.shanju1316@gmail.com>
2026-07-18 00:52:23 +0530

    Add LiveMarketState class with initial attributes

---
+++ engines/live_market_state.py
@@ -0,0 +1,15 @@
+class LiveMarketState:
+
+    def __init__(self):
+
+        self.nifty = {}
+
+        self.sensex = {}
+
+        self.option_chain = {}
+
+        self.vix = {}
+
+        self.future = {}
+
+        self.timestamp = None
